fd688f197ea05e8a9f2795291f59145a7da80d58
João Pereira <jp_ebp@hotmail.com>
2018-03-19 16:55:46 +0000

    asd

--- videoIntercom/ex2.js
+++ videoIntercom/ex2.js
@@ -1,5 +1,5 @@
 var spawn = require('child_process').spawn,
-child = spawn('/opt/vc/bin/raspivid', ['-t', '0', '-w', '800', '-h', '600', '-hf', '-fps', '30', '-o', '-' ]);
+child = spawn('/opt/vc/bin/raspivid', ['-t', '0', '-w', '800', '-h', '600', /*'-hf'*/, '-fps', '30', '-o', '-' ]);
 
 var http = require("http");
 var server = http.createServer(function(request, response) {
