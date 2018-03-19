4b36aac9057ef50f23923e7f5be334f4295140ba
João Pereira <jp_ebp@hotmail.com>
2018-03-19 17:13:49 +0000

    ffmpeg test

--- videoIntercom/ex2.js
+++ videoIntercom/ex2.js
@@ -1,6 +1,8 @@
 var spawn = require('child_process').spawn,
-child = spawn('/opt/vc/bin/raspivid', ['-t', '0', '-w', '800', '-h', '600', /*'-hf'*/, '-fps', '30', '-o', '-' ]);
+//child = spawn('/opt/vc/bin/raspivid', ['-t', '0', '-w', '800', '-h', '600', /*'-hf'*/, '-fps', '30', '-o', '-' ]);
 
+
+child = spawn('ffmpeg', ['-f' ,'v4l2', '-framerate', '25', '-video_size', '640x480', '-i','/dev/video0' , '$out']);
 var http = require("http");
 var server = http.createServer(function(request, response) {
 
